11bd131d553ef105034b10eb991f6007944eb42d
vmiseikis <vmiseikis@gmail.com>
2024-07-30 09:36:01 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://vmiseikis.github.io',
+  build: {
+    outDir: 'dist',
+  }
 })
